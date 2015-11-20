48c949c1503b2b9fd9613577a063d0493af894df
shaerdi <shaerdi@student.ethz.ch>
2015-11-20 06:59:53 +0100

    Euler Verfahren

---
+++ codes/explicitEuler.m
@@ -0,0 +1,15 @@
+t0 = 0;     % Startzeit:
+tend = 1;   % Simulieren bis
+h = 1e-3;   % Schrittweite
+x0 = 2;     % Startwert
+
+% DGL in der Form x' = f(t,x)
+f = @(t,x) 5x^2-1;
+
+% Schleife 
+x = x0;
+t = t0;
+while t<tend
+    t=t+h;
+    x = x+h*f(t,x);
+end
